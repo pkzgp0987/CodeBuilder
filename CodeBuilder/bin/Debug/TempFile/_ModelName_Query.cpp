f8c083258e9a6fe772728316e8febc3dd42aeeb8
pkzgp0987 <717864815@qq.com>
2014-11-14 18:41:38 +0800

    修改模版Char* str未初始化错误

--- CodeBuilder/bin/Debug/TempFile/_ModelName_Query.cpp
+++ CodeBuilder/bin/Debug/TempFile/_ModelName_Query.cpp
@@ -19,31 +19,35 @@ _ModelName_Query* _ModelName_Query::GetQuery()
 
 void _ModelName_Query::Insert(_ModelName_ *pEntity)
 {
-    char* str;
+    char* str= ( char* )malloc(1024);
     sprintf(str,"insert into _TableName_(<#foreach[table]case[0]:[_columnName_](,)#>) values (<#foreach[table]case[INTEGER,REAL,TEXT]:['%d';'%f';'%s'](,)#>)",<#foreach[table]case[INTEGER,REAL,TEXT]:[pEntity->_columnName_;pEntity->_columnName_;pEntity->_columnName_.c_str()](,)#>);
 	DBConnection::GetConnection()->ExecSQL(str,NULL,NULL);
 	pEntity->ID=DBConnection::GetConnection()->GetInsertID();
+	delete str;
 }
 
 void InsertWithID(_ModelName_ *pEntity)//插入一行数据
 {
-    char* str;
+    char* str= ( char* )malloc(1024);
     sprintf(str,"insert into _TableName_(<#foreach[table0]case[0]:[_columnName_](,)#>) values (<#foreach[table0]case[INTEGER,REAL,TEXT]:['%d';'%f';'%s'](,)#>)",<#foreach[table0]case[INTEGER,REAL,TEXT]:[pEntity->_columnName_;pEntity->_columnName_;pEntity->_columnName_.c_str()](,)#>);
 	DBConnection::GetConnection()->ExecSQL(str,NULL,NULL);
+	delete str;
 }
 
 void _ModelName_Query::Update(_ModelName_ *pEntity)
 {
-	char* str;
+	char* str= ( char* )malloc(1024);
     sprintf(str,"update _TableName_ set <#foreach[table]case[INTEGER,REAL,TEXT]:[_columnName_='%d';_columnName_='%f';_columnName_='%s'](,)#> where <#PIDcase[INTEGER,REAL,TEXT]:[_PID_='%d';_PID_='%f';_PID_='%s']#>",<#foreach[table0]case[INTEGER,REAL,TEXT]:[pEntity->_columnName_;pEntity->_columnName_;pEntity->_columnName_.c_str()](,)#>,<#PIDcase[INTEGER,REAL,TEXT]:[pEntity->_PID_;pEntity->_PID_;pEntity->_PID_.c_str()]#>);
 	DBConnection::GetConnection()->ExecSQL(str,NULL,NULL);
+	delete str;
 }
 
 void _ModelName_Query::DeleteByID(<#PIDcase[INTEGER,REAL,TEXT]:[int;float;const char*]#> ID)
 {
-	char* str;
+	char* str= ( char* )malloc(1024);
     sprintf(str,"delete from _TableName_ where <#PIDcase[INTEGER,REAL,TEXT]:[_PID_='%d';_PID_='%f';_PID_='%s']#>",ID);
 	DBConnection::GetConnection()->ExecSQL(str,NULL,NULL);
+	delete str;
 }
 
 int _ModelName_SelectCB( void * para, int n_column, char ** column_value, char ** column_name )
@@ -56,9 +60,10 @@ int _ModelName_SelectCB( void * para, int n_column, char ** column_value, char *
 _ModelName_ *_ModelName_Query::SelectByID(<#PIDcase[INTEGER,REAL,TEXT]:[int;float;const char*]#> ID)
 {
 	_ModelName_ *pEntity=new _ModelName_();
-	char* str;
+	char* str= ( char* )malloc(1024);
     sprintf(str,"select * from _TableName_ where <#PIDcase[INTEGER,REAL,TEXT]:[_PID_='%d';_PID_='%f';_PID_='%s']#>",ID);
 	DBConnection::GetConnection()->ExecSQL(str,_ModelName_SelectCB,pEntity);
+	delete str;
 	return pEntity;
 }
 
@@ -73,16 +78,17 @@ int _ModelName_SelectArrayCB( void * para, int n_column, char ** column_value, c
 std::list<_ModelName_*> SelectByWhere(const char* where)//查找所有数据
 {
     std::list<_ModelName_*> pArray;
-	char* str;
+	char* str= ( char* )malloc(1024);
     sprintf(str,"select * from _TableName_ where %s",where);
 	DBConnection::GetConnection()->ExecSQL(str,_ModelName_SelectArrayCB,&pArray);
+	delete str;
 	return pArray;
 }
 std::list<_ModelName_*> _ModelName_Query::SelectAll()
 {
 	std::list<_ModelName_*> pArray;
 	const char* str="select * from _TableName_";
-	DBConnection::GetConnection()->ExecSQL(str,_ModelName_SelectArrayCB,&pArray);
+	DBConnection::GetConnection()->ExecSQL(str,_ModelName_SelectArrayCB,&pArray);s
 	return pArray;
 }
 
